9b6e463a8fe655ff15cb38c83648f3512fe4be29
Robert Moore <robert.ben.moore@gmail.com>
2019-04-09 12:41:04 -0700

    Added docs

--- PeerIt/PeerIt/wwwroot/peerit/src/containers/portal.js
+++ PeerIt/PeerIt/wwwroot/peerit/src/containers/portal.js
@@ -2,10 +2,51 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import * as webix from "../webix/codebase/webix.js";
+
+
 //const webix = require("../webix/codebase/webix.js");
 //import ''
 import '../webix/codebase/webix.css';
 
+class Portal extends Component {
+  render() {
+    return (
+      <div ref="root"></div>
+    );
+  }
+
+  setWebixData(data){
+    const ui = this.ui;
+    if (ui.setValues)
+      ui.setValues(data);
+    else if (ui.parse)
+      ui.parse(data)
+    else if (ui.setValue)
+      ui.setValue(data); 
+  }
+
+  componentWillUnmount(){
+    this.ui.destructor();
+    this.ui = null;
+  }
+
+  componentWillUpdate(props){
+    if (props.data)
+      this.setWebixData(props.data);
+    if (props.select)
+      this.select(props.select);
+  }
+
+  componentDidMount(){
+  	this.ui = window.webix.ui(
+  	  this.props.ui, 
+  	  ReactDOM.findDOMNode(this.refs.root)
+	  );
+
+    this.componentWillUpdate(this.props);
+  }
+  
+}
 /* 
 class SliderView extends Component {
   render() {
@@ -67,4 +108,4 @@ const SliderView = () => (
   */
 
 //}
-export default SliderView;
+export default Portal;

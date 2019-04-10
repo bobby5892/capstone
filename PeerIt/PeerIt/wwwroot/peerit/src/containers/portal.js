1b95fed40a21c97a4d3aff7fd4e1ebebffd2a72c
Robert Moore <robert.ben.moore@gmail.com>
2019-04-09 19:53:08 -0700

    Working webix - Example

--- PeerIt/PeerIt/wwwroot/peerit/src/containers/portal.js
+++ PeerIt/PeerIt/wwwroot/peerit/src/containers/portal.js
@@ -1,111 +1,42 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-import * as webix from "../webix/codebase/webix.js";
-
-
-//const webix = require("../webix/codebase/webix.js");
-//import ''
-import '../webix/codebase/webix.css';
-
-class Portal extends Component {
-  render() {
-    return (
-      <div ref="root"></div>
-    );
-  }
-
-  setWebixData(data){
-    const ui = this.ui;
-    if (ui.setValues)
-      ui.setValues(data);
-    else if (ui.parse)
-      ui.parse(data)
-    else if (ui.setValue)
-      ui.setValue(data); 
-  }
-
-  componentWillUnmount(){
-    this.ui.destructor();
-    this.ui = null;
-  }
-
-  componentWillUpdate(props){
-    if (props.data)
-      this.setWebixData(props.data);
-    if (props.select)
-      this.select(props.select);
-  }
-
-  componentDidMount(){
-  	this.ui = window.webix.ui(
-  	  this.props.ui, 
-  	  ReactDOM.findDOMNode(this.refs.root)
-	  );
-
-    this.componentWillUpdate(this.props);
-  }
-  
-}
-/* 
-class SliderView extends Component {
-  render() {
-    return (
-      <div ref="root"></div>
-    );
-  }
-
-  componentDidMount(){
-    this.ui = window.webix.ui({
-      view:"slider",
-      container:ReactDOM.findDOMNode(this.refs.root)
+import Webix from '../webix';
+import FormView from './FormView';
+ 
+window.webix.protoUI({
+  name:"react",
+  defaults:{
+    borderless:true
+  },
+  $init:function(config){
+    this.$ready.push(function(){    
+      ReactDOM.render(
+        this.config.app,
+        this.$view
+      );
     });
   }
+}, window.webix.ui.view)
 
-  componentWillUnmount(){
-    this.ui.destructor();
-    this.ui = null;
-  }
 
-  shouldComponentUpdate(){
-    return false;
-  }
+function getForm(){
+  var subApp = <FormView></FormView>;
+
+  return {
+    view:"form", width:500, elements:[
+      { view:"text", name:"Company", label:"Name", placeholder:"Type your full name here"},
+      { type:"header", template:"Owner" },
+      {
+        view:"react", height: 220, app:subApp
+      },
+      { view:"label", label:"the above form is a separate React App inside of Webix UI" }
+    ]
+  };
 }
-*/ 
-//class Portal extends Component {
-/* Does not work const ui = {
-    view:"slider"
-};
-const value = 123;
- 
-const SliderView = () => (
-  <Webix ui={ui} data={value} />
+
+const Portal = ({ data, save }) => (
+  <div>
+    <Webix ui={getForm()} data={data}/>
+  </div>
 )
-*/
- /* Doesn't Work
- constructor(){
-    super();
-    this.state = {
-
-    }
-    this.webix = this.loadWebix();
-  }
-  render() {
-    return(
-      <div id="chart">blarg</div>
-        
-      
-    );
-  }
-  loadWebix(){
-    const container = window.getElementById("chart");
-    let tableConfig = {
-       data: ['test','test']
-    }
-    tableConfig.view = "datatable";
-    this._table = webix.ui(tableConfig, container);
-  }
-  */
-
-//}
 export default Portal;

92b2ef01cc53dc9b2adcd9797f6fee8a0cd44d53
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-05-07 12:39:00 -0700

    Not 100% sure what I am committing

--- Testing/test.js
+++ Testing/test.js
@@ -17,10 +17,20 @@ lwat.post("http://localhost:8080/Account/Login",{
 		lwat.assert(response.success,true,"Check That Login works");
 });
 
-
-
-lwat.get("http://localhost:8080/Account/GetCurrentUserRole").then((response) => { 
-		lwat.assert(response.data[0].role,"Administrator","Check That current role is admin");
+lwat.post("http://localhost:8080/Account/Login",{ 
+			"Email" : "admin@example.com",
+			"Password" : "password",
+			"returnUrl" : ""
+	}).then((response) => { 
+		lwat.get("http://localhost:8080/Account/GetCurrentUserRole").then((res) => { 
+			lwat.assert(res.data[0].role,"Administrator","Check That current role is admin");
+	});
 });
 
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\--Comment Controller Testing--/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+/*lwat.get("http://localhost:8080/Comment/GetCommentByUserId").then((response) => { 
+		lwat.assert(response.data[0].role,"Administrator","Check That current role is admin");
+});
+*/
 

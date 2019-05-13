596446fafe693375831610e1503a617936fc4918
Student <Student@1913100-C18079.lanecc.edu>
2019-05-13 14:01:09 -0700

    Migrations Cleanup

--- PeerIt/PeerIt/Migrations/20190513205545_Adjustments.Designer.cs
+++ PeerIt/PeerIt/Migrations/20190513205545_Adjustments.Designer.cs
@@ -10,8 +10,8 @@ using PeerIt.Repositories;
 namespace PeerIt.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    [Migration("20190511155439_Changes")]
-    partial class Changes
+    [Migration("20190513205545_Adjustments")]
+    partial class Adjustments
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -271,27 +271,21 @@ namespace PeerIt.Migrations
                         .ValueGeneratedOnAdd()
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
+                    b.Property<DateTime>("DueDate");
+
                     b.Property<int>("FK_COURSEID");
-
-                    b.Property<string>("InstructionText")
-                        .HasMaxLength(100000);
-
-                    b.Property<string>("InstructionsUrl")
-                        .HasMaxLength(200);
 
                     b.Property<string>("Name")
                         .IsRequired()
                         .HasMaxLength(60);
 
-                    b.Property<string>("RubricText")
-                        .HasMaxLength(100000);
-
-                    b.Property<string>("RubricUrl")
-                        .HasMaxLength(200);
+                    b.Property<string>("PFileID");
 
                     b.HasKey("ID");
 
                     b.HasIndex("FK_COURSEID");
+
+                    b.HasIndex("PFileID");
 
                     b.ToTable("CourseAssignments");
                 });
@@ -567,6 +561,10 @@ namespace PeerIt.Migrations
                         .WithMany()
                         .HasForeignKey("FK_COURSEID")
                         .OnDelete(DeleteBehavior.Restrict);
+
+                    b.HasOne("PeerIt.Models.PFile", "PFile")
+                        .WithMany()
+                        .HasForeignKey("PFileID");
                 });
 
             modelBuilder.Entity("PeerIt.Models.CourseGroup", b =>
--- PeerIt/PeerIt/Migrations/20190513205545_Adjustments.cs
+++ PeerIt/PeerIt/Migrations/20190513205545_Adjustments.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace PeerIt.Migrations
 {
-    public partial class Changes : Migration
+    public partial class Adjustments : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropColumn(
                 name: "Content",
                 table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "InstructionText",
+                table: "CourseAssignments");
+
+            migrationBuilder.DropColumn(
+                name: "InstructionsUrl",
+                table: "CourseAssignments");
+
+            migrationBuilder.DropColumn(
+                name: "RubricText",
+                table: "CourseAssignments");
+
+            migrationBuilder.DropColumn(
+                name: "RubricUrl",
+                table: "CourseAssignments");
 
             migrationBuilder.AddColumn<string>(
                 name: "FK_PFileID",
@@ -19,6 +36,17 @@ namespace PeerIt.Migrations
             migrationBuilder.AddColumn<string>(
                 name: "ReviewGroup",
                 table: "CourseGroups",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "CourseAssignments",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "PFileID",
+                table: "CourseAssignments",
                 nullable: true);
 
             migrationBuilder.CreateTable(
@@ -47,9 +75,22 @@ namespace PeerIt.Migrations
                 column: "FK_PFileID");
 
             migrationBuilder.CreateIndex(
+                name: "IX_CourseAssignments_PFileID",
+                table: "CourseAssignments",
+                column: "PFileID");
+
+            migrationBuilder.CreateIndex(
                 name: "IX_PFiles_AppUserId",
                 table: "PFiles",
                 column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseAssignments_PFiles_PFileID",
+                table: "CourseAssignments",
+                column: "PFileID",
+                principalTable: "PFiles",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
 
             migrationBuilder.AddForeignKey(
                 name: "FK_Reviews_PFiles_FK_PFileID",
@@ -63,6 +104,10 @@ namespace PeerIt.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropForeignKey(
+                name: "FK_CourseAssignments_PFiles_PFileID",
+                table: "CourseAssignments");
+
+            migrationBuilder.DropForeignKey(
                 name: "FK_Reviews_PFiles_FK_PFileID",
                 table: "Reviews");
 
@@ -73,6 +118,10 @@ namespace PeerIt.Migrations
                 name: "IX_Reviews_FK_PFileID",
                 table: "Reviews");
 
+            migrationBuilder.DropIndex(
+                name: "IX_CourseAssignments_PFileID",
+                table: "CourseAssignments");
+
             migrationBuilder.DropColumn(
                 name: "FK_PFileID",
                 table: "Reviews");
@@ -81,12 +130,44 @@ namespace PeerIt.Migrations
                 name: "ReviewGroup",
                 table: "CourseGroups");
 
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "CourseAssignments");
+
+            migrationBuilder.DropColumn(
+                name: "PFileID",
+                table: "CourseAssignments");
+
             migrationBuilder.AddColumn<string>(
                 name: "Content",
                 table: "Reviews",
                 maxLength: 100000,
                 nullable: false,
                 defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "InstructionText",
+                table: "CourseAssignments",
+                maxLength: 100000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InstructionsUrl",
+                table: "CourseAssignments",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RubricText",
+                table: "CourseAssignments",
+                maxLength: 100000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RubricUrl",
+                table: "CourseAssignments",
+                maxLength: 200,
+                nullable: true);
         }
     }
 }

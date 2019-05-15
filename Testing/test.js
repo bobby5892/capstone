35ee0b8558a865dbef4455f45bf7fe475234d729
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-05-15 14:58:30 -0700

    Readded the tests

--- Testing/test.js
+++ Testing/test.js
@@ -47,3 +47,67 @@ lwat.post("http://localhost:8080/Course/CreateCourse",
 			console.log(JSON.stringify(response.data[0]));
 			//lwat.assert(response.data[0].)
 		});
+///\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/Comment Controller Tests\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+//Testing GetCommentsByUserId with a user that has no comments
+lwat.post("http://localhost:8080/Account/Login",{ 
+			"Email" : "admin@example.com",
+			"Password" : "password",
+			"returnUrl" : ""
+	}).then((response) => {
+		//console.log(JSON.stringify(response));
+        lwat.get("http://localhost:8080/Comment/GetCommentsByUserId").then((res) => {
+		   lwat.assert(res.error[0].name,"No Comments","Check that user with no comments returns 'no comments'");
+        });
+	});
+	
+//Testing GetCommentsByAssignmentId with ivalid assignment id
+lwat.post("http://localhost:8080/Account/Login",{
+	"Email":"admin@example.com",
+	"Password":"password",
+	"returnUrl":""
+}).then((response) => {
+	lwat.get("http://localhost:8080/Comment/GetCommentsByAssignmentId?assignmentId:4").then((res)=>{	
+	lwat.assert(res.error[0].name,"Null Assignment at Id","Null Assignment at Id");
+	});
+});
+//Testing Comment count for user when the user has no comments
+lwat.post("http://localhost:8080/Account/Login",{
+	"Email":"admin@example.com",
+	"Password":"password",
+	"returnUrl":""
+}).then((response) => {
+	lwat.get("http://localhost:8080/Comment/GetCommentCountForUser").then((res)=>{	
+	lwat.assert(res.totalResults,0, "comment count for user");
+	});
+});
+//Testing comment count for assignment when assignment has no comments
+lwat.post("http://localhost:8080/Account/Login",{
+	"Email":"admin@example.com",
+	"Password":"password",
+	"returnUrl":""
+}).then((response) => {
+	lwat.get("http://localhost:8080/Comment/GetCommentCountForAssignment").then((res)=>{	
+		lwat.assert(res.totalResults,0, "Comment Count for assignment");
+	});
+});
+//Testing Create Comment when failed because assignment was null
+lwat.post("http://localhost:8080/Account/Login",{
+	"Email":"admin@example.com",
+	"Password":"password",
+	"returnUrl":""
+}).then((response) => {
+	lwat.get("http://localhost:8080/Comment/CreateComment").then((res)=>{	
+		lwat.assert(res.error[0].description, "No student assignment for the given Id", "Test Create Comment");
+	});
+});
+//Testing Delete Comment when failed because assignment was null
+lwat.post("http://localhost:8080/Account/Login",{
+	"Email":"admin@example.com",
+	"Password":"password",
+	"returnUrl":""
+}).then((response) => {
+	lwat.get("http://localhost:8080/Comment/DeleteComment").then((res)=>{	
+		lwat.assert(res.error[0].description, "No Comment for given Id","Test Delete Comment");
+	});
+});
+
